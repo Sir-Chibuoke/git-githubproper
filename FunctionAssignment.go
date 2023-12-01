3c59e6ab8001d272a8477717563e99a202f73a83
Chibuoke Uchechukwu Emmanuel <Chibuokeu@gmail.com>
2023-12-01 12:24:04 +0100

    Our Assignment

---
+++ FunctionAssignment.go
@@ -0,0 +1,25 @@
+// A function in Go that takes two integers as parameters and returns their sum.
+// package main
+
+// import "fmt"
+
+// func Assignment1(x int, y int) int {
+// 	return x + y
+// }
+// func main() {
+
+// 	fmt.Println(Assignment1(2, 3))
+// }
+
+// A function that accepts a variable number of strings and concatenates them into a single string
+package main
+
+import "fmt"
+
+func Assignment2(A string, B string) string {
+	return A + B
+}
+func main() {
+	fmt.Println(Assignment2("Samba ", "institute"))
+
+}
